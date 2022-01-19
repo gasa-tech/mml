3cb4d26320cd7b8c3c7e5a3a4b08b71d85c49fc3
reancirl <reancirl@gmail.com>
2022-01-19 22:35:14 +0800

    initial commit reacts scaffold

---
+++ src/App.js
@@ -0,0 +1,25 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Case sensitive siya bantog mo error pag compile
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          SORRY HA!
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
